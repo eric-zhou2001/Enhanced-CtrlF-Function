53adce6ef3a0c8e7362f2b0a27f696ddccbdd386
Na1ve51699 <44799109+Na1ve51699@users.noreply.github.com>
2020-09-29 10:46:47 +0800

    Update input() and remove print()

--- backendP1/Backend1.java
+++ backendP1/Backend1.java
@@ -39,15 +39,19 @@ public class Backend1 {
     public void input(String newWord){
         if(this.table.containsKey(newWord)){//if the word exists, add 1 to its occurrence
             Integer tempValue=this.table.get(newWord);
-            this.getHashNode(newWord).setValue(tempValue+1);
-            //we have to write a new function to get the hash node because get() only return an integer
+            for(int i=0;i<data.getHashNodeList().size();i++){
+                if(data.getHashNodeList().get(i).getKey()==newWord){//update the hash node list in data process
+                    data.getHashNodeList().get(i).setValue(tempValue+1);
+                    break;
+                }
+            }
+            this.table.loopToNode(newWord).setValue(tempValue+1);
+            //the loopToNode function should be public
         }
         else{//if the word doesn't exist, put a new hash node into the table
             this.table.put(newWord,1);
+            data.getHashNodeList().add(new HashNode<String, Integer>(newWord, 1));//update the hash node list in data process
         }
     }
 // Note from meeting: Data Wrangler I implementation already has printall() function.
-    public void print(){
-        //there should be a function to iterate our hash table in HashTableMap.java
-    }
 }
